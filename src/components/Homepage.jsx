a1d4913bad7a07da19bfb4aa2913cdc5f0bf7d7c
Mateus Sobrinho <chocsx@MacBook-Pro-de-chocsx.local>
2021-12-27 09:14:11 -0300

    fix exchange page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,42 +1,44 @@
-import React from 'react'
-import { Typography, Row, Col, Statistic } from 'antd'
+import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
 
-import { useGetCryptosQuery } from '../services/cryptoApi'
-import millify from 'millify'
-import { Link } from 'react-router-dom'
-import { Cryptocurrencies, News } from '.'
+import { useGetCryptosQuery } from '../services/cryptoApi';
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
 import Loader from './Loader';
 
-const { Title } = Typography
+const { Title } = Typography;
 
 const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
 
-  if(isFetching) return <Loader />;
+  if (isFetching) return <Loader />;
 
   return (
     <>
-      <Title level={2} className='heading'>Global Crypto Stats</Title>
-      <Row>
+      <Title level={2} className="heading">Global Crypto Stats</Title>
+      <Row gutter={[32, 32]}>
         <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
-        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
-        <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/></Col>
-        <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
-        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
-      <div className='home-heading-container'>
-        <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the world</Title>
-        <Title level={3} className='show-mode'><Link to='/cryptocurrencies'>Show More</Link></Title>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
       </div>
-      <Cryptocurrencies simplified/>
-      <div className='home-heading-container'>
-        <Title level={2} className='home-title'>Last Crypto in News</Title>
-        <Title level={3} className='show-mode'><Link to='/news'>Show More</Link></Title>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3}><Link to="/news">Show more</Link></Title>
       </div>
-      <News simplified/>
+      <News simplified />
     </>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import { Provider } from "react-redux";
 
 import App from './App';
 import store from "./app/store";
-import 'antd/dist/antd.min.css';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
+  <React.StrictMode>
   <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </Router>,
+  </Router>
+</React.StrictMode>,
  document.getElementById('root'))
